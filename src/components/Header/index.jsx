9d424a30a2b06ae0ab7863936f5d050e8668cf38
rabbitning <s111019023@stu.ntue.edu.tw>
2023-04-08 23:21:50 +0800

    header 1.0 complete

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,17 +4,21 @@ import { Link } from 'react-router-dom'
 export default function Header() {
     return (
         <header>
-            <div className={styles.navigator}>
-                <Link to='/' className={styles.navItem}>Home</Link>
-                <Link to='/' className={styles.navItem}>genre</Link>
-                <Link to='/' className={styles.navItem}>contact</Link>
-            </div>
-            <div className={styles.searchBox}>
-                <input type="text" placeholder='search' className={styles.searchBar} />
-            </div>
-            <div className='usercorner'>
-                <Link to='/'></Link>
-                <Link to='/'></Link>
+            <div className={styles.headerContent}>
+                <div className={styles.navigator}>
+                    <Link to='/' className={`${styles.navHomeBtn} ${styles.navItem} mainHover`}>Home</Link>
+                    <Link to='/' className={`${styles.navItem} mainHover`}>genre</Link>
+                    <Link to='/' className={`${styles.navItem} mainHover`}>contact</Link>
+                </div>
+                <div className={styles.searchBox}>
+                    <input type="text" placeholder='search' className={styles.searchBar} />
+                </div>
+                <div className={styles.usercorner}>
+                    <Link to='/' className={`${styles.postBtn} subHover`}>post</Link>
+                    <Link to='/' className={`${styles.userIcon} subHover`}>
+                        <i className='fas fa-user-circle'></i>
+                    </Link>
+                </div>
             </div>
         </header>
     )
